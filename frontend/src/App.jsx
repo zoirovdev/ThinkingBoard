bdfd220b5b20111909d02c5c1eff4751f0ee61b3
zoirovdev <abboszoirov17@gmail.com>
2025-07-30 11:48:20 +0500

    Finish HomePage.jsx UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ import toast from "react-hot-toast"
 
 const App = () => {
   return (
-    <div data-theme="forest">
+    <div className="relative h-full w-full" >
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage/>}/>
 	<Route path="/create" element={<CreatePage/>}/>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,57 @@
 import Navbar from "../components/Navbar.jsx"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import RateLimitedUI from "../components/RateLimitedUI.jsx"
+import axios from "axios"
+import toast from "react-hot-toast"
+import NoteCard from "../components/NoteCard.jsx"
+
 
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true)
+  const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes")
+	console.log(res.data)
+	setNotes(res.data)
+	setIsRateLimited(false)
+      } catch (error) {
+        console.log("Error fetching notes")
+	console.log(error)
+	if(error.response?.status === 429){
+	  setIsRateLimited(true)
+	} else {
+	  toast.error("Failed to load notes")
+	}
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchNotes()
+  }, [])
 
   return (
     <div className="min-h-screen">
       <Navbar />
 
       {isRateLimited && <RateLimitedUI/>}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+	{notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note}/>
+	    ))}
+	  </div>
+	)}
+      </div>
     </div>
   )
 }
