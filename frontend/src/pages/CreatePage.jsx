a1a8ffb0e66ed776001fd02253700ef7ddaf346a
zoirovdev <abboszoirov17@gmail.com>
2025-08-04 00:09:27 +0500

    Update CreatePage.jsx

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -46,36 +46,36 @@ const CreatePage = () => {
   return (
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
-        <div className="max-w-2xl mx-auto">
+        <div className="max-w-xl mx-auto">
           <Link to={"/"} className="btn btn-ghost mb-6">
-            <ArrowLeftIcon className="size-5" />
+            <ArrowLeftIcon className="size-5 mr-2" />
             Back to Notes
           </Link>
 
           <div className="card bg-base-100">
             <div className="card-body">
-              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <h2 className="card-title text-2xl mb-6">Create New Note</h2>
               <form onSubmit={handleSubmit}>
-                <div className="form-control mb-4">
+                <div className="form-control mb-6 flex flex-col space-y-2">
                   <label className="label">
                     <span className="label-text">Title</span>
                   </label>
                   <input
                     type="text"
                     placeholder="Note Title"
-                    className="input input-bordered"
+                    className="input input-bordered w-[100%]"
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                   />
                 </div>
 
-                <div className="form-control mb-4">
+                <div className="form-control mb-6 flex flex-col space-y-2">
                   <label className="label">
                     <span className="label-text">Content</span>
                   </label>
                   <textarea
                     placeholder="Write your note here..."
-                    className="textarea textarea-bordered h-32"
+                    className="textarea textarea-bordered h-32 w-[100%]"
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
                   />
